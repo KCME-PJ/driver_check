670ae5b83427f159f4d2b0442d114fd77b7423f7
kcme-pjKCME-PJ <ken-fujita@kcme.jp>
2024-06-27 17:05:30 +0900

    ログイン認証設定

---
+++ user/login_check.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$email = filter_input(INPUT_POST, 'email');
+$pass = filter_input(INPUT_POST, 'pass');
+
+require_once '../db_access/database.php';
+require_once '../function/session_validation.php';
+
+$user_valid = session_vali($email, $pass);
+if ($user_valid == 0) {
+    header('Location: ./login_err.html');
+} else {
+    header('Location: ../index.php');
+}

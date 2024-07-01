6d08ded75a041f0e302be70c73161cebaf13945c
kcme-pjKCME-PJ <ken-fujita@kcme.jp>
2024-07-01 16:14:30 +0900

    ログインユーザーの取得情報追加

--- function/session_user.php
+++ function/session_user.php
@@ -13,7 +13,8 @@ function session_user($mail)
     $l_name = $user_info['l_name'];
     $f_name = $user_info['f_name'];
     $d_id = $user_info['d_id'];
+    $access_auth = $user_info['access_authority'];
 
     $dbh = null;
-    return array($l_name, $f_name, $d_id);
+    return array($l_name, $f_name, $d_id, $access_auth);
 }

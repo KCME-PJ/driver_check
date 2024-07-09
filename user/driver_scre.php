b42ef0e86df63e12aa228eb3543e0dd8fa6bb766
kcme-pjKCME-PJ <ken-fujita@kcme.jp>
2024-07-09 11:25:17 +0900

    navbarを追加

--- user/driver_scre.php
+++ user/driver_scre.php
@@ -7,6 +7,8 @@
     <title>ドライバー安全レベルチェック結果</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -33,7 +35,7 @@
                                 <i class="bi bi-person-fill"></i>&nbsp;<?php echo "$name さん"; ?>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item" href="./user/logout.php">Logout</a></li>
+                                <li><a class="dropdown-item" href="./logout.php">Logout</a></li>
                             </ul>
                         </li>
                     </ul>

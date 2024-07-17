7e2598af6d3276d735764d635596bb2ec3556ce8
KCME-PJ <ken-fujita@kcme.jp>
2024-07-17 09:45:11 +0900

    テスト結果のページにテスト実施のリンクを追加

--- user/index.php
+++ user/index.php
@@ -40,6 +40,7 @@
                                 <i class="bi bi-person-fill"></i>&nbsp;<?php echo "$name さん"; ?>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="../index.php">テスト実施</a></li>
                                 <li><a class="dropdown-item" href="./logout.php">Logout</a></li>
                             </ul>
                         </li>

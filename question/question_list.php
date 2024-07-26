9bd8426de07885a7a7fa42d3964a19e2a8639b99
KCME-PJ <ken-fujita@kcme.jp>
2024-07-26 14:52:45 +0900

    navbarの表示修正

--- question/question_list.php
+++ question/question_list.php
@@ -42,7 +42,7 @@
                 <div class="navbar-left">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page">ドライバー登録</a>
+                            <a class="nav-link active" aria-current="page">設問リスト</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">

978bef7fa378709f60ea4e35e649c34c848e2d88
KCME-PJ <ken-fujita@kcme.jp>
2024-07-29 11:46:58 +0900

    formの空欄対策を追加

--- user/user_update.php
+++ user/user_update.php
@@ -1,11 +1,11 @@
 <?php
-$driver_id = filter_input(INPUT_POST, 'driver_id');
+$driver_id = filter_input(INPUT_POST, 'driver_id') ?? null; //$_POST['driver_id']の存在をチェック、なければnullを代入
 $lname = filter_input(INPUT_POST, 'lname');
 $fname = filter_input(INPUT_POST, 'fname');
 $auth = filter_input(INPUT_POST, 'auth');
 $email = filter_input(INPUT_POST, 'address');
 $d_id = filter_input(INPUT_POST, 'd_id');
-
+$driver_id = empty($driver_id) ? null : $driver_id;  //$_POST['driver_id']に有効な値があるかをチェック、空文字などであればnullを代入
 require_once '../db_access/database.php';
 $sql = <<<SQL
 UPDATE drivers SET driver_id = ?, access_authority = ?, f_name = ?, l_name = ?, email = ? WHERE d_id = ?

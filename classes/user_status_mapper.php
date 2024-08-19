1f555e54041db61739edc62b89072010c0962d57
KCME-PJ <ken-fujita@kcme.jp>
2024-08-19 11:17:48 +0900

    update処理を追加

--- classes/user_status_mapper.php
+++ classes/user_status_mapper.php
@@ -18,4 +18,37 @@ class UserStatusMapper
         }
         return $users;
     }
+    public function findById($d_id)
+    {
+        $sql_findUser = <<<SQL
+        SELECT * FROM drivers WHERE d_id = :d_id
+        SQL;
+        $stmt = $this->pdo->prepare($sql_findUser);
+        $stmt->execute(['d_id' => $d_id]);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        return new UserStatus($data);
+    }
+    public function driverUpdate(UserStatus $userStatus) //driver情報アップデート処理
+    {
+        $d_id = $userStatus->getId();                   //UserStatus classのgetterから値を取得
+        $driver_id = $userStatus->getDriverId();
+        $auth = $userStatus->getAccessLevel();
+        $employee = $userStatus->getEmployeeNumber();
+        $f_name = $userStatus->getFname();
+        $l_name = $userStatus->getLname();
+        $email = $userStatus->getEmail();
+        $sql_update = <<<SQL
+        UPDATE drivers SET driver_id = :driver_id, access_authority = :auth, employee_number = :employee, f_name = :f_name, l_name = :l_name, email = :email
+        WHERE d_id = :d_id
+        SQL;
+        $stmt = $this->pdo->prepare($sql_update);
+        $stmt->bindParam(':driver_id', $driver_id);
+        $stmt->bindParam(':auth', $auth);
+        $stmt->bindParam(':employee', $employee);
+        $stmt->bindParam(':f_name', $f_name);
+        $stmt->bindParam(':l_name', $l_name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':d_id', $d_id);
+        return $stmt->execute();
+    }
 }
